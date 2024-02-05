901085170d62abeb007916c027acb0a5785217e8
jrson83 <me@jrson.de>
2024-02-05 16:10:41 +0100

    build: replace vitest with native test runner

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,15 +1,16 @@
-import type { RehypeSlugAnchorSectionizeOptions } from '../src'
+import rehypeSlugSectionizeAnchorize, {
+  type RehypeSlugAnchorSectionizeOptions,
+} from '../src'
 
-import { expect, it } from 'vitest'
-
+import { deepStrictEqual } from 'node:assert/strict'
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
+import { it } from 'node:test'
 
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
-import rehypeSlugSectionizeAnchorize from '../src'
 
 const cwd = process.cwd()
 const path = (file: string) => resolve(cwd, 'test', 'markdown', file)
@@ -29,5 +30,8 @@ it('should ...', async () => {
     await readFile(path('markdown.md'), { encoding: 'utf8' })
   )
 
-  expect(value).toMatchSnapshot()
+  deepStrictEqual(
+    value,
+    '<section id="heading-21-section"><h2 id="heading-21" role="presentation"><a href="#heading-21" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.1</span></h2>\n<p>Paragraph 1</p>\n</section><section id="heading-22-section"><h2 id="heading-22" role="presentation"><a href="#heading-22" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.2</span></h2>\n<p>Paragraph 2</p>\n<section id="heading-31-section"><h3 id="heading-31" role="presentation"><a href="#heading-31" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="3">Heading 3.1</span></h3>\n<p>Paragraph 3</p>\n</section></section><section id="heading-23-section"><h2 id="heading-23" role="presentation"><a href="#heading-23" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.3</span></h2>\n<p>Paragraph 4</p></section>'
+  )
 })
