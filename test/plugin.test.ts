197391eae4a7bf1c8dcab7742b8c0f6df6ad95ac
jrson83 <github@jrson.de>
2025-02-08 02:52:43 +0100

    test: improve test

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -5,7 +5,7 @@ import rehypeSlugSectionizeAnchorize, {
 import { deepStrictEqual } from 'node:assert/strict'
 import { readFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
-import { it } from 'node:test'
+import { describe, it } from 'node:test'
 
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
@@ -25,13 +25,15 @@ function createProcessor(options?: RehypeSlugAnchorSectionizeOptions) {
   return processor
 }
 
-it('should ...', async () => {
-  const { value } = await createProcessor().process(
-    await readFile(path('markdown.md'), { encoding: 'utf8' })
-  )
+describe('plugin test', () => {
+  it('should render headings with default options', async () => {
+    const { value } = await createProcessor().process(
+      await readFile(path('markdown.md'), { encoding: 'utf8' })
+    )
 
-  deepStrictEqual(
-    value,
-    '<section id="heading-21-section"><h2 id="heading-21" role="presentation"><a href="#heading-21" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.1</span></h2>\n<p>Paragraph 1</p>\n</section><section id="heading-22-section"><h2 id="heading-22" role="presentation"><a href="#heading-22" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.2</span></h2>\n<p>Paragraph 2</p>\n<section id="heading-31-section"><h3 id="heading-31" role="presentation"><a href="#heading-31" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="3">Heading 3.1</span></h3>\n<p>Paragraph 3</p>\n</section></section><section id="heading-23-section"><h2 id="heading-23" role="presentation"><a href="#heading-23" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.3</span></h2>\n<p>Paragraph 4</p></section>'
-  )
+    deepStrictEqual(
+      value,
+      '<section id="heading-21-section"><h2 id="heading-21" role="presentation"><a href="#heading-21" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.1</span></h2>\n<p>Paragraph 1</p>\n</section><section id="heading-22-section"><h2 id="heading-22" role="presentation"><a href="#heading-22" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.2</span></h2>\n<p>Paragraph 2</p>\n<section id="heading-31-section"><h3 id="heading-31" role="presentation"><a href="#heading-31" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="3">Heading 3.1</span></h3>\n<p>Paragraph 3</p>\n</section></section><section id="heading-23-section"><h2 id="heading-23" role="presentation"><a href="#heading-23" class="heading-anchor"><button type="button" class="heading-anchor__btn">#</button></a><span role="heading" aria-level="2">Heading 2.3</span></h2>\n<p>Paragraph 4</p></section>'
+    )
+  })
 })
